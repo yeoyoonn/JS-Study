dd1e7f7ab342a87cbb79964833504a92caf1c569
yeoyoonn <18610148980@163.com>
2022-02-03 15:12:57 +0900

    문제7해결!

--- week01/main.js
+++ week01/main.js
@@ -2,12 +2,12 @@
 'use strict';
 import { question, questionInt } from 'readline-sync';
 // 문제1
-// const firstName = question("이름을 입력하세요");
+// const firstName = question("이름을 입력하세요.\n");
 // console.log("hello" + firstName);
 
 //문제2
-// const firstName = question("이름을 입력하세요");
-// const lastName = question("성을 입력하세요");
+// const firstName = question("이름을 입력하세요.\n");
+// const lastName = question("성을 입력하세요.\n");
 // console.log("%s %s", firstName, lastName);
 
 // 문제3
@@ -25,10 +25,13 @@ import { question, questionInt } from 'readline-sync';
 // const thridNum = questionInt("세번째 숫자를 입력하세요.\n");
 // console.log('The total is %d', (firstNum + secdNum)*thridNum);
 
-문제6
-const firstNum = questionInt("처음의 피자 조각 개수를 입력하세요.\n");
-const secdNum = questionInt("몇 조각 먹었는지 개수를 입력하세요.\n");
-console.log('지금 남은 피자 조각 수는 %d입니다.', firstNum - secdNum);
+//문제6
+// const firstNum = questionInt("처음의 피자 조각 개수를 입력하세요.\n");
+// const secdNum = questionInt("몇 조각 먹었는지 개수를 입력하세요.\n");
+// console.log("지금 남은 피자 조각 수는 %d입니다.", firstNum - secdNum);
 
-
+//문제7
+const firstName = question("이름을 입력하세요.\n");
+const age = questionInt("나이를 입력하세요.\n") + 1;
+console.log("%s next birthday you will be %d", firstName, age);
 
