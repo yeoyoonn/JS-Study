104b9031cd0bea7b4b909efa4707e3e0fc4e955e
yeoyoonn <18610148980@163.com>
2022-02-03 14:57:15 +0900

    문제 4해결!

--- week01/main.js
+++ week01/main.js
@@ -1,5 +1,21 @@
 
-import { question } from 'readline-sync';
-const name = question("이름을 입력하세요")
+'use strict';
+import { question, questionInt } from 'readline-sync';
+// 문제1
+// const firstName = question("이름을 입력하세요");
+// console.log("hello" + firstName);
 
-console.log("hello" + name)
+//문제2
+// const firstName = question("이름을 입력하세요");
+// const lastName = question("성을 입력하세요");
+// console.log(firstName +" "+ lastName);
+
+// 문제3
+// console.log("What do you call a bear with no teeth?\nA gummy bear!");
+
+
+// 문제4
+const firstNum = questionInt("첫번째 숫자를 입력하세요.\n");
+const secdNum = questionInt("두번째 숫자를 입력하세요.\n");
+console.log('The total is %d', firstNum + secdNum);
+
