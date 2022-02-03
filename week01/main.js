e35722e7e2ae34d2441e82577f081484e54dad8b
yeoyoonn <18610148980@163.com>
2022-02-03 14:59:47 +0900

    문제5해결!

--- week01/main.js
+++ week01/main.js
@@ -15,7 +15,14 @@ import { question, questionInt } from 'readline-sync';
 
 
 // 문제4
+// const firstNum = questionInt("첫번째 숫자를 입력하세요.\n");
+// const secdNum = questionInt("두번째 숫자를 입력하세요.\n");
+// console.log('The total is %d', firstNum + secdNum);
+
+// 문제5
+
 const firstNum = questionInt("첫번째 숫자를 입력하세요.\n");
 const secdNum = questionInt("두번째 숫자를 입력하세요.\n");
-console.log('The total is %d', firstNum + secdNum);
+const thridNum = questionInt("세번째 숫자를 입력하세요.\n");
+console.log('The total is %d', (firstNum + secdNum)*thridNum);
 
