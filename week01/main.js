fdf352ccd13fa31a088e6eb1aae6a9f0bd2c8138
yeoyoonn <18610148980@163.com>
2022-02-03 15:07:03 +0900

    문제6해결!

--- week01/main.js
+++ week01/main.js
@@ -8,7 +8,7 @@ import { question, questionInt } from 'readline-sync';
 //문제2
 // const firstName = question("이름을 입력하세요");
 // const lastName = question("성을 입력하세요");
-// console.log(firstName +" "+ lastName);
+// console.log("%s %s", firstName, lastName);
 
 // 문제3
 // console.log("What do you call a bear with no teeth?\nA gummy bear!");
@@ -20,9 +20,15 @@ import { question, questionInt } from 'readline-sync';
 // console.log('The total is %d', firstNum + secdNum);
 
 // 문제5
+// const firstNum = questionInt("첫번째 숫자를 입력하세요.\n");
+// const secdNum = questionInt("두번째 숫자를 입력하세요.\n");
+// const thridNum = questionInt("세번째 숫자를 입력하세요.\n");
+// console.log('The total is %d', (firstNum + secdNum)*thridNum);
 
-const firstNum = questionInt("첫번째 숫자를 입력하세요.\n");
-const secdNum = questionInt("두번째 숫자를 입력하세요.\n");
-const thridNum = questionInt("세번째 숫자를 입력하세요.\n");
-console.log('The total is %d', (firstNum + secdNum)*thridNum);
+문제6
+const firstNum = questionInt("처음의 피자 조각 개수를 입력하세요.\n");
+const secdNum = questionInt("몇 조각 먹었는지 개수를 입력하세요.\n");
+console.log('지금 남은 피자 조각 수는 %d입니다.', firstNum - secdNum);
+
+
 
